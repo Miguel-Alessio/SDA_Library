6fd2d38651dbd5c2dc417d2e69a8f820fe0c9b1d
Pasagali Alessio <essio.pg@gmail.com>
2026-03-08 19:47:51 +0200

    main.c

--- main.c
+++ main.c
@@ -2,45 +2,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-typedef struct Node{
+typedef struct Node
+{
     int data;
-    struct Node* next;
-}Node;
+    struct Node *next;
+} Node;
 
-Node* createNote(int val){
-    Node* newNode=(Node*)malloc(sizeof(Node));
-    if(newNode==NULL){
+Node *createNode(int val)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode == NULL)
+    {
         printf("Eroare: Nu s-a putut aloca memorie.\n");
         return NULL;
     }
-    newNode -> data = val;
-    newNode -> next = NULL;
+    newNode->data = val;
+    newNode->next = NULL;
     return newNode;
 }
 
-void insertAtHead(int val,Node** head_ref){
-    Node* newNode=createNote(val);
-    newNode -> next=*head_ref;
-    *head_ref=newNode;
+void push(int val, Node **top_ref)
+{
+    Node *newNode = createNode(val);
+    newNode->next = *top_ref;
+    *top_ref = newNode;
 }
 
-void printList(Node* head){
-    Node* temp=head;
-    while(temp!=NULL){
-        printf("%d -> ", temp->data);
-        temp=temp->next;
+int pop(Node **top_ref)
+{
+    if (*top_ref == NULL)
+    {
+        printf("Stack Underflow! Nothing to pop.\n");
+        return -1;
+    }
+    Node *temp = *top_ref;
+    int poppedValue = temp->data;
+    *top_ref = (*top_ref)->next;
+    free(temp);
+    return poppedValue;
+}
+
+int peek(Node *top)
+{
+    if (top == NULL)
+    {
+        printf("Stack is empty.\n");
+        return -1;
+    }
+    return top->data;
+}
+
+void printStack(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        printf("[%d] -> ", temp->data);
+        temp = temp->next;
     }
     printf("NULL\n");
 }
 
-int main(){
+int main()
+{
 
-    Node* head=NULL;
-    insertAtHead(30,&head);
-    insertAtHead(20,&head);
-    insertAtHead(10,&head);
+    Node *stack = NULL;
+    int choice=0, val;
 
-    printList(head);
+    printf("--- SDA Library: Stack Project ---\n");
+    printf("Enter numbers to push (any non-number to stop ---)\n");
+    while (scanf("%d", &val) == 1)
+    {
+        push(val, &stack);
+    }
+    // This clears the input buffer
+while (getchar() != '\n');
+    printStack(stack);
+
+    printf("Do you want to pop any number?\nIf yes write how many, if not than just type '0':");
+    scanf("%d", &choice);
+
+    
+    if(choice<0){
+        printf("Only positive numbers: \n");
+        scanf("%d",&choice);
+    }
+    
+    if(choice==0){
+        return 0;
+    }
+
+    if (choice > 0)
+    {
+        while (choice>0)
+        {
+            printf("\nPeeking at the top: %d\n", peek(stack));
+
+            printf("Popping the top element: %d\n", pop(&stack));
+            choice--;
+
+        }
+    }
+
+
+    printStack(stack);
+
     return 0;
 }
