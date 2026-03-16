966ed81c29bee14a4bbbf92fa292d7883b2ee20d
Pasagali Alessio <93616175+Miguel-Alessio@users.noreply.github.com>
2026-03-16 18:28:40 +0200

    Refactor singly linked list to use TList structure
    Refactor singly linked list implementation to use TList structure. Update functions for initialization, insertion, printing, and freeing the list.

--- 01_Lists/singly.c
+++ 01_Lists/singly.c
@@ -1,38 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
 
-Node* createNode(int val) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    if (newNode == NULL) return NULL;
-    newNode->data = val;
-    newNode->next = NULL;
-    return newNode;
+typedef struct Node {
+    void *data;
+    struct Node *next;
+} Node;
+
+typedef struct TList {
+    Node *head;
+    int len;
+} TList;
+
+void init(TList **list) {
+    *list = (TList *)malloc(sizeof(TList));
+    if (*list != NULL) {
+        (*list)->head = NULL;
+        (*list)->len = 0;
+    }
 }
 
-void insertHead(Node** head, int val) {
-    Node* newNode = createNode(val);
-    if (newNode == NULL) return;
-    newNode->next = *head;
-    *head = newNode;
+int insertHead(TList *list, void *data) {
+    if (list == NULL) return -1;
+
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode == NULL) return -1;
+    
+    newNode->data = data;
+    newNode->next = list->head;
+    list->head = newNode;
+    list->len++;
+    
+    return 1;
 }
 
-void printList(Node* head) {
-    Node* temp = head;
+Node *pop(TList *list) {
+    if (list == NULL || list->head == NULL) return NULL;
+
+    Node *temp = list->head;
+    list->head = list->head->next;
+    list->len--;
+    
+    return temp;
+}
+
+void print_int_list(TList *list) {
+    if (list == NULL) return;
+    
+    Node *temp = list->head;
     while (temp != NULL) {
-        printf("[%d] -> ", temp->data);
+        printf("[%d] -> ", *(int *)(temp->data));
         temp = temp->next;
     }
     printf("NULL\n");
 }
 
-int pop(Node** head,int val){
-    if(*head==NULL)return -1;
-    Node* temp=*head;
-    int val= temp->data;
-    *head=(*head)->next;
+void free_list(TList **list) {
+    if (!list || !*list) return;
+    
+    Node *curr = (*list)->head;
+    while (curr) {
+        Node *aux = curr;
+        curr = curr->next;
+        free(aux);
+    }
+    
+    free(*list);
+    *list = NULL;
+}
 
-    free(temp);
-    return val;
+int main() {
+    TList *list;
+    init(&list);
 
+    int a = 10, b = 20, c = 30;
+    
+    insertHead(list, &a);
+    insertHead(list, &b);
+    insertHead(list, &c);
+
+    print_int_list(list);
+
+    Node *removed = pop(list);
+    if (removed != NULL) {
+        free(removed);
+    }
+
+    print_int_list(list);
+
+    free_list(&list);
+
+    return 0;
 }
